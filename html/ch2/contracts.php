f8063dbe0dd53a3fef0d8fbb0f34187de2c5bbf9
Pitikorn <manee-mn@hotmail.com>
2018-11-08 14:20:21 +0700

    add ch2 file contract.php

---
+++ html/ch2/contracts.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>contracts</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  </head>
+  <body>
+      <div class="container">
+        <h1>
+        <?php
+            if(isset($_GET['url'])){
+                $url=$_GET['url'];
+                if($url == "https://www.evil.c0m"){
+                    echo "flag{0pen_Redir3ct}";
+                }
+                echo "<script>window.location.href = '$url';</script>";
+            }
+        ?>
+        </h1>
+      <h1>Contract</h1>
+      <h6>Phone: 0888888888</h6>
+      <h6>Email: wreck_it_lab@mail.com</h6>
+      <h1>Address</h1>
+      <h6> 999/999 Disney st. Bong dist. Thailand 10000</h6>
+      <h1>Social</h1>
+      <a style="color:#3968b8" href="?url=https://www.facebook.com/"><i style="" class="fab fa-facebook-square"></i> Facebook</a>
+      <a style="color:#00a2f8" href="?url=https://www.twitter.com/"><i class="fab fa-twitter-square"></i> Twitter</a>
+      <a style="color:red" href="?url=https://www.youtube.com/"><i class="fab fa-youtube"></i> Youtube</a>
+      </div>
+
+    
+   
+  </body>
+</html>
