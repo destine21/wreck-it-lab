c0522eb325ccb492fe2d7189c8dd964a58f33f77
bengri <kbengri@gmail.com>
2018-11-09 16:18:35 +0700

    add bootstap , header

---
+++ header_home.php
@@ -0,0 +1,32 @@
+<?php require_once('header.php') ?>
+
+    <header>
+        <img src="" alt="">
+        <h1>wreck-it-lab</h1>
+        <div class="nav-banner d-flex">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </div>
+    </header>
+    <div class="d-flex">
+        <div class="sidebar-banner d-block">
+            <ul>
+                <a href="#"><li>Client-side Authentication </li></a>
+                <a href="#"><li>Open redirect </li></a>
+                <a href="#"><li>HTTP POST request </li></a>
+                <a href="#"><li>Insecure Direct Object Reference </li></a>
+                <a href="#"><li>Privilege escalation (via cookie) </li></a>
+                <a href="#"><li>Directory listing </li></a>
+                <a href="#"><li>Weak Cryptography </li></a>
+                <a href="#"><li>Local file inclusion </li></a>
+                <a href="#"><li>SQL Injection </li></a>
+                <a href="#"><li>XSS </li></a>
+                <a href="#"><li>Insecure file upload </li></a>
+                <a href="#"><li>OS command Injection </li></a>
+                <a href="#"><li>Insecure serialization </li></a>
+            </ul>
+        </div>
+
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php require_once('header.php') ?>
+<?php require_once('header_home.php') ?>
+
+    <div class="content-banner">
+        <h1>HTTP Headers</h1>
+         <div class="sub-header">
+             <h3>15 point</h3>
+             <h4>HTTP response give informations</h4>
+         </div>
+         <div class="detail-cha">
+             <h5>Statement</h5>
+             <p>Get an administrator access to the webpage.</p>
+             <a href="#" class="btn btn-primary" role="button">Start the challenge</a>
+             <h5>Validation</h5>
+             <p>Enter password :</p>
+             <input type="text">
+             <a href="" class="btn btn-primary" role="button">Send</a>
+             <h5>2 related ressource(s)</h5>
+             <ul>
+                 <li><a href="#">test1</a></li>
+                 <li><a href="#">test2</a></li>
+             </ul>
+             <ul class="accordion" id="investAccordion">
+                <li>
+                    <h2 class="invest-collapse collapsed" id="headingOne" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Hint</h2>
+                    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#investAccordion">
+                        -
+                    </div>    
+                </li>
+                <li>
+                    <h2 class="invest-collapse collapsed" id="headingTwo" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">Solution</h2>
+                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#investAccordion">
+                        -
+                    </div>    
+                </li>
+            </ul>            
+         </div>
+    </div>
+</div>
