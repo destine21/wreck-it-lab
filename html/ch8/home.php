28671cddc24db1a6eee40ff4a9b3aea154acae1d
Pitikorn <manee-mn@hotmail.com>
2018-11-09 18:40:16 +0700

    add ch8 fix flag file in ch6

---
+++ html/ch8/home.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>A</h1>
+  </body>
+</html>
---
+++ html/ch8/pages.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<?php $flag = "test"; ?>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  </head>
+  <body>
+
+  <h1>File</h1>&nbsp;|&nbsp;
+  <a href="?page=home.php">Home</a>&nbsp;|&nbsp;
+  <a href="?page=about.php">About</a>&nbsp;|&nbsp;
+  <a href="?page=contract.php">Contract</a>&nbsp;|&nbsp;
+  
+  <?php
+  if(isset($_GET['page'])){
+    $page = $_GET['page'];
+    include($page); 
+  }
+  ?>
+    
+  </body>
+</html>
