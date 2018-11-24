9c2e6da75c30b676516b82a256eb9106e65bbd0e
h0me <destine21ep2@gmail.com>
2018-11-24 14:16:16 +0700

    add ch10 fakebook xss

---
+++ html/ch10/post.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$dbname = "fakebook";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// prepare and bind
+$stmt = $conn->prepare("INSERT INTO `posts` (`id`, `content`, `author`, `time`) VALUES (NULL, ?, 'tester', CURRENT_TIMESTAMP)");
+$stmt->bind_param("s", $content);
+
+// set parameters and execute
+$content = $_POST['content'];
+$stmt->execute();
+
+echo "New records created successfully";
+
+header( "location: fakebook.php" );
+
+$stmt->close();
+$conn->close();
+?>
