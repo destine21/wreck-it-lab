a071404dfefe42aa8e3ff7911403092ab3c933e2
h0me <destine21ep2@gmail.com>
2018-11-27 18:21:23 +0700

    change password ch1

--- html/ch1/login1.js
+++ html/ch1/login1.js
@@ -1,8 +1,8 @@
 function authentication(){
 	var username=document.login.username.value;
     var password=document.login.password.value;
-	if (username=="felix" && password=="flag{Javascr1pt_is_s0_s3cure}") {
-	    alert("Congratulation, you can use this password as a flag to archive score!");
+	if (username=="felix" && password=="XWj66uPR2gyYpwmS") {
+	    alert("Congratulation, your flag is '" + atob("ZmxhZ3tKYXZhc2NyMXB0X2lzX3MwX3MzY3VyZX0=") + "' you can use this flag to archive score!");
 	} else { 
 	    alert("Username or password are incorrect!"); 
 	}
