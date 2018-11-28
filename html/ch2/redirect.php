d1f8ef59d6fde078ba9bed674796c5da745957dd
h0me <destine21ep2@gmail.com>
2018-11-28 18:29:36 +0700

    improve ch2

--- html/ch2/redirect.php
+++ html/ch2/redirect.php
@@ -2,9 +2,9 @@
     if(isset($_GET['url'])){
         $url=$_GET['url'];
         echo "Redirecting...";
-        if($url == "https://www.evil.c0m"){
-            echo "flag{It's_a_0pen_Redir3ct}";
+        if(substr( $url, 0, 20 ) === "https://www.evil.c0m"){
+            echo "<h1>flag{It's_a_0pen_Redir3ct}</h1>";
         }
-        echo "<script>setTimeout(()=>{window.location.href = '$url';},1000)</script>";
+        echo "<script>setTimeout(()=>{window.location.href = '$url';},500)</script>";
     }
 ?>
