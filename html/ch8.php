61c98131905150df8121053aac900f2f40801990
h0me <destine21ep2@gmail.com>
2018-11-30 14:25:09 +0700

    fix ch8 path

--- html/ch8.php
+++ html/ch8.php
@@ -17,7 +17,7 @@
                     <div>
                         <h5>Goal</h5>
                         <p>Bypass login and access to the webpage.</p>
-                        <a href="ch8/pages.php" target="blank" class="btn btn-primary" role="button">Start the Lab</a>            
+                        <a href="ch8/pages.php?page=home.php" target="blank" class="btn btn-primary" role="button">Start the Lab</a>            
                     </div>
                     <div>
                         <h5>Validation</h5>
