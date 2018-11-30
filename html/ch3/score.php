22cc86cff545f87c0b4013589cca31c8cba817a7
bengri <kbengri@gmail.com>
2018-11-30 16:08:54 +0700

    no message

--- html/ch3/score.php
+++ html/ch3/score.php
@@ -1,22 +1,16 @@
 <?php require_once('../header_ch.php'); ?>
 
 <div class="container">
-  <!-- <div class="hammer">
-    <img src="images/hammer.gif" alt="">
-<<<<<<< HEAD
-  </div> -->
+  <div class="title p-1 position-relative text-light">
+    <h1>Beat the High Score !!!</h1>
+    <h3>High Score : 100</h3>
+  </div>
   <div class="score">
-    <!-- <h1>Beat the score</h1>
-    <h4>Try to beat the high score to win this.</h4>
-    <h6>How to play :</h6>
-    <ul class="mb-4">
-      <li>Click on the button to hope to generate a great score</li>
-      <li>Highest score is <strong>100</strong></li>
-    </ul> -->
     <div class="d-flex">
       <div class="m-5">
         <?php 
-           if(isset($_POST["score"])){
+            $score = 0;
+            if(isset($_POST["score"])){
              $score = $_POST["score"];
                if($_POST["score"] < 100){
                   echo "<div class='wrong-score'><p>" ;
@@ -43,42 +37,9 @@
       <div class="hammer">
         <img src="images/hammer.gif" alt="">
       </div>
-=======
-  </div>
-  <div class="title container p-1 position-absolute text-light">
-    <h1>Beat the High Score !!!</h1>
-    <h3>High Score : 100</h3>
-  </div>
-  <div class="score">
-    <?php 
-        $score = 0;
-         if(isset($_POST["score"])){
-           $score = $_POST["score"];
-             if($_POST["score"] < 100){
-                echo "<div class='wrong-score'><p>" ;
-                echo $_POST["score"] ;
-                echo "</p></div>" ;
-             }else{
-                echo "<div class='pass-score'><p>";
-                echo $_POST["score"];
-                echo "</p> ";
-                echo "<h1>Here is your flag : <strong>Th1s_i5_H7TP_P0S7_R3que5t</strong></h1> </div>";
-             }
-        }
-    
-        ?>
-    <div class="mt-3">
-      <input orient='270deg' type='range' min='0' value=<?php echo $score; ?> max='110' step='20' disabled/>
-      <form action="" method="post" onsubmit="document.getElementsByName('score')[0].value = Math.floor(Math.random() * 99)"
-        class="mt-3 text-center">
-        <input type="hidden" name="score" value="-1" />
-        <input type="submit" name="generate" value="HIT!">
-      </form>
->>>>>>> 47bfb8d5cd8635d6a4d559d3512d75534b25e6db
     </div>
   </div>
-</div>
 
-</body>
+  </body>
 
-</html>
+  </html>
