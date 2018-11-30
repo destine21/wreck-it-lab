e91490cbb4408108ac6fb9f1b0e03f3a6819f6a4
bengri <kbengri@gmail.com>
2018-11-30 15:56:56 +0700

    add style ch6 edit style ch3

--- html/ch3/score.php
+++ html/ch3/score.php
@@ -1,9 +1,9 @@
 <?php require_once('../header_ch.php'); ?>
 
 <div class="container">
-  <div class="hammer">
+  <!-- <div class="hammer">
     <img src="images/hammer.gif" alt="">
-  </div>
+  </div> -->
   <div class="score">
     <!-- <h1>Beat the score</h1>
     <h4>Try to beat the high score to win this.</h4>
@@ -12,29 +12,36 @@
       <li>Click on the button to hope to generate a great score</li>
       <li>Highest score is <strong>100</strong></li>
     </ul> -->
-    <?php 
-         if(isset($_POST["score"])){
-           $score = $_POST["score"];
-             if($_POST["score"] < 100){
-                echo "<div class='wrong-score'><p>" ;
-                echo $_POST["score"] ;
-                echo "</p></div>" ;
-             }else{
-                echo "<div class='pass-score'><p>";
-                echo $_POST["score"];
-                echo "</p> ";
-                echo "<h1>Here is your flag : <strong>Th1s_i5_H7TP_P0S7_R3que5t</strong></h1> </div>";
-             }
-        }
-    
-        ?>
-    <div class="mt-3">
-      <input orient='270deg' type='range' min='0' value=<?php echo $score; ?> max='110' step='20' disabled/>
-      <form action="" method="post" onsubmit="document.getElementsByName('score')[0].value = Math.floor(Math.random() * 99)"
-        class="mt-3 text-center">
-        <input type="hidden" name="score" value="-1" />
-        <input type="submit" name="generate" value="HIT!">
-      </form>
+    <div class="d-flex">
+      <div class="m-5">
+        <?php 
+           if(isset($_POST["score"])){
+             $score = $_POST["score"];
+               if($_POST["score"] < 100){
+                  echo "<div class='wrong-score'><p>" ;
+                  echo $_POST["score"] ;
+                  echo "</p></div>" ;
+               }else{
+                  echo "<div class='pass-score'><p>";
+                  echo $_POST["score"];
+                  echo "</p> ";
+                  echo "<h1>Here is your flag : <strong>Th1s_i5_H7TP_P0S7_R3que5t</strong></h1> </div>";
+               }
+          }
+      
+          ?>
+      </div>
+      <div class="mt-3">
+        <input orient='270deg' type='range' min='0' value=<?php echo $score; ?> max='110' step='20' disabled/>
+        <form action="" method="post" onsubmit="document.getElementsByName('score')[0].value = Math.floor(Math.random() * 99)"
+          class="mt-3 text-center">
+          <input type="hidden" name="score" value="-1" />
+          <input type="submit" name="generate" value="HIT!">
+        </form>
+      </div>
+      <div class="hammer">
+        <img src="images/hammer.gif" alt="">
+      </div>
     </div>
   </div>
 </div>
