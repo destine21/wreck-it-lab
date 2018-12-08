2f2316b7d3cab5cee408bbcef88a9c1996346172
Pitikorn <manee-mn@hotmail.com>
2018-12-09 01:37:47 +0700

    edit title and link css ch1

--- html/ch1/login1.php
+++ html/ch1/login1.php
@@ -1,89 +1,13 @@
-<?php require_once('../header_ch.php'); ?>
-<style>
-@import url(https://fonts.googleapis.com/css?family=Open+Sans);
-        * {
-        padding: 0;
-        margin: 0;
-        }
+<?php require_once '../header_ch.php'; ?>
 
-        html {
-        font-family: Open Sans;
-        overflow: hidden;
-        }
-        .Page1 {
-        z-index: 2;
-        position: relative;
-        background: url("Fixit.jpeg") center;
-        background-size: 100vw 100vh;
-        width: 100vw;
-        height: 100vh;
-        }
-        .Page1 h1 {
-        position: relative;
-        color: #FFF;
-        font-size: 5em;
-        font-family: Open Sans;
-        top: 65vh;
-        }
-
-        .Page2 {
-        z-index: 1;
-        position: relative;
-        background: url("fixit_bg.jpg") no-repeat;
-        background-size: 100vw 100vh;
-        width: 100vw;
-        height: 100vh;
-        top: -100vh;
-        }
-
-        .userLogo {
-        width: 15em;
-        height: 15em;
-        position: relative;
-        margin: 0em auto;
-        display: block;
-        }
-        .userLogo img {
-        width: 15em;
-        height: 15em;
-        border-radius: 10em;
-        }
-
-        .userName {
-        text-align: center;
-        color: #FFF;
-        margin: 1em 0em 1em 0em;
-        }
-
-        .userEmail {
-        text-align: center;
-        color: #FFF;
-        margin: 1em 0em 1em 0em;
-        }
-
-        .passwordInput {
-        position: relative;
-        margin: 0 auto;
-        display: block;
-        font-size: 2em;
-        width: 10em;
-        outline: none;
-        border: rgba(204, 204, 204, 0.7) solid;
-        }
-
-        .ch1-wrapper{
-
-        }
-
-</style>
 <!-- <div class="bg-main"> -->
 <html>
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
     <link rel="stylesheet" href="../assets/fontAwesome/css/all.css">
 </head>
 <body>
@@ -100,7 +24,7 @@
             </div>
         </div>
         <div class="Page2">
-            <div class="userLogo"><img class="mt-2" src="Felix.jpg"/></div>
+            <div class="userLogo"><img class="mt-2" src="img/Felix.jpg"/></div>
             <div class="userEmail">
                         <div class="container">
                     <div class="ch1-wrapper w-50 mx-auto pt-3">
