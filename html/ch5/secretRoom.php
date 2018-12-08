0e04afb070f97d6895af97818ec6629595e8e68f
bengri <kbengri@gmail.com>
2018-12-08 13:43:58 +0700

    add ch5 style1

--- html/ch5/secretRoom.php
+++ html/ch5/secretRoom.php
@@ -1,55 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require_once('../header_ch.php') ?>
+<title>Secret Room</title>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../assets/plugins/bootstrap/css/bootstrap.min.css">
-
-    <title>Secret Room</title>
-
-    <style>
-        .cover{
-            height: 100vh;
-            margin: 0px;
-        }
-        img.sign{
-            margin: 10px;
-            width: 20%;
-            background-color: #fff;
-            border-radius: 50%;
-        }
-    </style>
-</head>
-
+<div class="container">
+    <div class="text-center">
 <?php
     if(!isset($_COOKIE['user'])){
         setcookie("user", "UmFscGg6Z3Vlc3Q=", time() + (86400 * 30), ".");
     }else{
         $userData = urldecode(base64_decode($_COOKIE['user']));
         $role = explode(":", $userData)[1];
-        if($role === "admin"){
-            echo '<div class="jumbotron cover text-center">
-            <h1 class="display-4">Secret Room!</h1>
-            <p class="lead">Wellcome back Administrator</p>
-            <h3>here is your flag <i>flag{Co0kies_!s_So_Yummy~}</i></h3>
-            <hr class="my-4">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur magni doloribus voluptas quisquam, molestiae esse dolores repudiandae reprehenderit vel quod laboriosam, pariatur aspernatur! Maiores, perferendis repellat veritatis assumenda suscipit voluptate!</p>
-        </div>';
-            die;
+        if($role === "admin"){ ?>
+
+        <h1 class="display-4">Secret Room!</h1>
+        <p class="lead">Wellcome back Administrator</p>
+        <h3>here is your flag <i>flag{Co0kies_!s_So_Yummy~}</i></h3>
+        <hr class="my-4">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur magni doloribus voluptas quisquam,
+            molestiae esse dolores repudiandae reprehenderit vel quod laboriosam, pariatur aspernatur! Maiores,
+            perferendis repellat veritatis assumenda suscipit voluptate!</p>
+    
+
+<?php  die;
         }
     } 
 ?>
 
-<body>
-    <div class="jumbotron cover text-center">
-        <h1 class="display-4">Secret Room!</h1>
-        <img src="images/stop.png" alt="" class="sign">
-        <p class="lead">YOU ARE NOT ALLOW TO GET IN HERE !! THIS PLACE IS ONLY FOR ADMIN !!</p>
+        <!-- <h1 class="display-4">Secret Room!</h1> -->
+        <div class="sign p-5">
+            <div class="m-auto"><img src="images/asdf.jpg" alt="" class="w-100"></div>
+            <h2>เว็บไซต์นี้มีเนื้อหาและข้อมูลที่ไม่เหมาะสม<br>ถูกระงับโดยกระทรวงเวทมนต์</h2>
+        </div>
+        <!-- <p class="lead">YOU ARE NOT ALLOW TO GET IN HERE !! THIS PLACE IS ONLY FOR ADMIN !!</p>
         <hr class="my-4">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur magni doloribus voluptas quisquam, molestiae esse dolores repudiandae reprehenderit vel quod laboriosam, pariatur aspernatur! Maiores, perferendis repellat veritatis assumenda suscipit voluptate!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur magni doloribus voluptas quisquam,
+            molestiae esse dolores repudiandae reprehenderit vel quod laboriosam, pariatur aspernatur! Maiores,
+            perferendis repellat veritatis assumenda suscipit voluptate!</p> -->
     </div>
-</body>
+</div>
 
 </html>
