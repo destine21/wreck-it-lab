a76e03fa2b54746eb06bfe2b26d284b15339c541
h0me <destine21ep2@gmail.com>
2018-12-08 14:16:03 +0700

    add serve.sh

---
+++ serve.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+
+echo '[+] Starting mysql...'
+service mysql start
+
+echo '[+] Starting apache'
+service apache2 start
+
+#XSS admin bot every 10 sec
+while sleep 10; do phantomjs --ignore-ssl-errors=true --local-to-remote-url-access=true --web-security=false --ssl-protocol=any /var/www/html/ch10/admin_auto_run.js; done &
+
+while true
+do
+    tail -f /var/log/apache2/*.log
+    exit 0
+done
