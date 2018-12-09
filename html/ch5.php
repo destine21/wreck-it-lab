1e0764f496073c7d88a1a392746dfa25de6d3148
h0me <destine21ep2@gmail.com>
2018-12-09 23:32:15 +0700

    rename ch5 to cornhub

--- html/ch5.php
+++ html/ch5.php
@@ -31,7 +31,7 @@
                     <div>
                         <h5>Goal</h5>
                         <p>Bypass login and access to the webpage.</p>
-                        <a href="ch5/secretRoom.php" target="blank" class="btn btn-primary" role="button">Start the Lab</a>            
+                        <a href="ch5/cornhub.php" target="blank" class="btn btn-primary" role="button">Start the Lab</a>            
                     </div>
                     <div>
                         <h5>Validation</h5>
